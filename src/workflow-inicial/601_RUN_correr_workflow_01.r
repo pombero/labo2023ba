cfc01aa95d566c80c59bcbcfbe7eb716530bb93c
pombero <ricardobarbieri@gmail.com>
2023-09-08 15:42:39 +0000

    601 - set source file

--- src/workflow-inicial/601_RUN_correr_workflow_01.r
+++ src/workflow-inicial/601_RUN_correr_workflow_01.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023ba/src/workflow-inicial/z611_CA_reparar_dataset.r")
-source("~/labo2023ba/src/workflow-inicial/z621_DR_corregir_drifting.r")
-source("~/labo2023ba/src/workflow-inicial/z631_FE_historia.r")
-source("~/labo2023ba/src/workflow-inicial/z641_TS_training_strategy.r")
+source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_01.r")
+source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_01.r")
+source("~/labo2023ba/src/workflow-inicial/631_FE_historia_01.r")
+source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_01.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-inicial/z651_HT_lightgbm.r")
-source("~/labo2023ba/src/workflow-inicial/z661_ZZ_final.r")
+source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_01.r")
+source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_01.r")
